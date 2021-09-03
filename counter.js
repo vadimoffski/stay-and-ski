f10883f536497cc59e1515f83dd22c047cc7b39a
vadimoffski <hryshchenko95@gmail.com>
2021-09-03 19:48:46 +0300

    added blocks and counter

---
+++ counter.js
@@ -0,0 +1,14 @@
+let counters = document.querySelectorAll('.item__number');
+counters.forEach((counter)=> {
+    counter.innerText = '0';
+    let updateCounter = () => {
+        let target = +counter.getAttribute('data-target');
+        let c = +counter.innerText;
+        let increment = target/2000;
+        if ( c< target){
+            counter.innerText = `${Math.ceil(c + increment)}`;
+            setTimeout( updateCounter, 1000);
+        }
+    };
+    updateCounter();
+});
